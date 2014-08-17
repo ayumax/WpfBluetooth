f18f619fb6d95426dadb1f6b6809052dd1fd8cbb
ayupantsu <ayuma0913@gmail.com>
2014-08-17 15:40:21 +0900

    入れ忘れ

---
+++ BluetoothPhone/Bluetooth/PhoneStatus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BluetoothPhone.Bluetooth
+{
+    public class PhoneStatus
+    {
+        public enum PhoneStatusKind
+        {
+            phoneNormal = 0,            // 待ち受け状態
+            phoneIncomingCalling,       // 着信中
+            phoneOutgoingCalling,       // 発信中
+            phoneConnect,               // 通話中
+        }
+
+        public PhoneStatusKind Status { get; set; }
+    }
+}
